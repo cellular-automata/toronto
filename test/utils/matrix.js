51facf27f7f2d26f630f7d67cf75bbb31b26918d
KAD <kad@dyn1247-204.vpn.ic.ac.uk>
2016-01-12 22:11:11 +0000

    Added getCellNeighbour() to Matrix class.

--- test/utils/matrix.js
+++ test/utils/matrix.js
@@ -2,17 +2,146 @@ const assert = require('assert');
 
 describe('Matrix Class', function () {
 
-  it('should return matrix class', function (done) {
-    const Matrix = require('../../utils/matrix');
-    assert(Matrix);
-    done();
+  describe('constructor()', function () {
+
+    it('should return matrix class', function (done) {
+      const Matrix = require('../../utils/matrix');
+      assert(Matrix);
+      done();
+    });
+
+    it('Matrix of 10x10 should have area of 100', function (done) {
+      const Matrix = require('../../utils/matrix');
+      const m = new Matrix(10, 10);
+      assert.equal(m.size, 10 * 10);
+      done();
+    });
+
+    it('should create a matrix with all zeros', function (done) {
+      const Matrix = require('../../utils/matrix');
+      const m = new Matrix(3, 3, 0);
+      assert.equal(m.getCell(0, 0), 0);
+      assert.equal(m.getCell(0, 1), 0);
+      assert.equal(m.getCell(0, 2), 0);
+      assert.equal(m.getCell(1, 0), 0);
+      assert.equal(m.getCell(1, 1), 0);
+      assert.equal(m.getCell(1, 2), 0);
+      assert.equal(m.getCell(2, 0), 0);
+      assert.equal(m.getCell(2, 1), 0);
+      assert.equal(m.getCell(2, 2), 0);
+      done();
+    });
+
+    it('should create a matrix with nine cells', function (done) {
+      const Matrix = require('../../utils/matrix');
+      const m = new Matrix(3, 3, [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+      ]);
+      assert.deepEqual(m.array, [1, 2, 3, 4, 5, 6, 7, 8, 9 ]);
+      done();
+    });
+
   });
 
-  it('Matrix of 10x10 should have area of 100', function (done) {
-    const Matrix = require('../../utils/matrix');
-    const m = new Matrix(10, 10);
-    assert.equal(m.area, 100);
-    done();
+  describe('setCells()', function () {
+
+    it.skip('should return an error', function (done) {
+      const Matrix = require('../../utils/matrix');
+      const m = new Matrix(3, 3);
+      m.setCells([1, 2]);
+      done();
+    });
+
+    it('should set cells array', function (done) {
+      const Matrix = require('../../utils/matrix');
+      const m = new Matrix(3, 3);
+      const a = [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+      ];
+      m.setCells(a);
+      assert.equal(m.array, a);
+      assert.equal(m.getCell(0, 0), 1);
+      assert.equal(m.getCell(0, 1), 2);
+      assert.equal(m.getCell(0, 2), 3);
+      assert.equal(m.getCell(1, 0), 4);
+      assert.equal(m.getCell(1, 1), 5);
+      assert.equal(m.getCell(1, 2), 6);
+      assert.equal(m.getCell(2, 0), 7);
+      assert.equal(m.getCell(2, 1), 8);
+      assert.equal(m.getCell(2, 2), 9);
+      done();
+    });
+
+  });
+
+  describe('getCellNeighbours()', function () {
+
+    it('should get centre cell neighbours', function (done) {
+      const Matrix = require('../../utils/matrix');
+      const m = new Matrix(3, 3, [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+      ]);
+      assert.deepEqual(m.getCellNeighbours(1, 1), [1, 2, 3, 4, 6, 7, 8, 9]);
+      done();
+    });
+
+    it('should get corner cell neighbours', function (done) {
+      const Matrix = require('../../utils/matrix');
+      const m = new Matrix(3, 3, [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+      ]);
+      assert.deepEqual(m.getCellNeighbours(0, 0), [9, 7, 8, 3, 2, 6, 4, 5]);
+      done();
+    });
+
+  });
+
+  describe('getCellNeighbour()', function () {
+
+    it('should get centre cell neighbour', function (done) {
+      const Matrix = require('../../utils/matrix');
+      const m = new Matrix(3, 3, [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+      ]);
+      assert.equal(m.getCellNeighbour(1, 1, 0), 1);
+      assert.equal(m.getCellNeighbour(1, 1, 1), 2);
+      assert.equal(m.getCellNeighbour(1, 1, 2), 3);
+      assert.equal(m.getCellNeighbour(1, 1, 3), 4);
+      assert.equal(m.getCellNeighbour(1, 1, 4), 6);
+      assert.equal(m.getCellNeighbour(1, 1, 5), 7);
+      assert.equal(m.getCellNeighbour(1, 1, 6), 8);
+      assert.equal(m.getCellNeighbour(1, 1, 7), 9);
+      done();
+    });
+
+    it('should get corner cell neighbour', function (done) {
+      const Matrix = require('../../utils/matrix');
+      const m = new Matrix(3, 3, [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+      ]);
+      assert.equal(m.getCellNeighbour(0, 0, 0), 9);
+      assert.equal(m.getCellNeighbour(0, 0, 1), 7);
+      assert.equal(m.getCellNeighbour(0, 0, 2), 8);
+      assert.equal(m.getCellNeighbour(0, 0, 3), 3);
+      assert.equal(m.getCellNeighbour(0, 0, 4), 2);
+      assert.equal(m.getCellNeighbour(0, 0, 5), 6);
+      assert.equal(m.getCellNeighbour(0, 0, 6), 4);
+      assert.equal(m.getCellNeighbour(0, 0, 7), 5);
+      done();
+    });
+
   });
 
 });
--- utils/matrix.js
+++ utils/matrix.js
@@ -1,10 +1,20 @@
 "use strict";
 
 class Matrix {
-  constructor(width, height) {
+  constructor(width, height, array) {
     this.width = width;
     this.height = height;
-    this.array = new Array(this.size);
+    if (Array.isArray(array)) {
+      this.setCells(array);
+    }
+    else {
+      this.array = new Array(this.size);
+      if (array !== undefined) {
+        for (var i = 0; i < this.array.length; i++) {
+          this.array[i] = array;
+        }
+      }
+    }
   }
 
   get lastRow() {
@@ -31,7 +41,21 @@ class Matrix {
     this.array[this.width * x + y] = value;
   }
 
-  getCellNeighbors(x, y) {
+  setCells(array) {
+    if (Array.isArray(array)) {
+      if (this.size === array.length) {
+        this.array = array;
+      }
+      else {
+        throw 'Invalid array size.';
+      }
+    }
+    else {
+      throw 'Invalid array.';
+    }
+  }
+
+  getCellNeighbours(x, y) {
     const previousRow = x === 0 ? this.lastRow : x - 1;
     const nextRow = x === this.lastRow ? 0 : x + 1;
     const previousColumn = y === 0 ? this.lastColumn : y - 1;
@@ -46,6 +70,29 @@ class Matrix {
       this.getCell(nextRow, y),
       this.getCell(nextRow, nextColumn)
     ];
+  }
+
+  getCellNeighbour(x, y, n) {
+    switch (n) {
+      case 0:
+        return this.getCell(x === 0 ? this.lastRow : x - 1, y === 0 ? this.lastColumn : y - 1);
+      case 1:
+        return this.getCell(x === 0 ? this.lastRow : x - 1, y);
+      case 2:
+        return this.getCell(x === 0 ? this.lastRow : x - 1, y === this.lastColumn ? 0 : y + 1);
+      case 3:
+        return this.getCell(x, y === 0 ? this.lastColumn : y - 1);
+      case 4:
+        return this.getCell(x, y === this.lastColumn ? 0 : y + 1);
+      case 5:
+        return this.getCell(x === this.lastRow ? 0 : x + 1, y === 0 ? this.lastColumn : y - 1);
+      case 6:
+        return this.getCell(x === this.lastRow ? 0 : x + 1, y);
+      case 7:
+        return this.getCell(x === this.lastRow ? 0 : x + 1, y === this.lastColumn ? 0 : y + 1);
+      default:
+        throw `Invalid Neighbour index: ${n}.`;
+    }
   }
 
   static copy(a, b) {
