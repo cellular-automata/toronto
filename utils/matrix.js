6993e96cd960681e6edbecf1d330d5185d243398
hequba <mia@monicutsmacbook.lan>
2016-01-12 20:10:48 +0000

    Added cell neighbours.

--- utils/matrix.js
+++ utils/matrix.js
@@ -7,8 +7,16 @@ class Matrix {
     this.array = new Array(this.size);
   }
 
+  get lastRow() {
+    return this.width - 1;
+  }
+
+  get lastColumn() {
+    return this.height - 1;
+  }
+
   get size() {
-    return this.calcSize()
+    return this.calcSize();
   }
 
   calcSize() {
@@ -21,6 +29,23 @@ class Matrix {
 
   setCell(x, y, value) {
     this.array[this.width * x + y] = value;
+  }
+
+  getCellNeighbors(x, y) {
+    const previousRow = x === 0 ? this.lastRow : x - 1;
+    const nextRow = x === this.lastRow ? 0 : x + 1;
+    const previousColumn = y === 0 ? this.lastColumn : y - 1;
+    const nextColumn = y === this.lastColumn ? 0 : y + 1;
+    return [
+      this.getCell(previousRow, previousColumn),
+      this.getCell(previousRow, y),
+      this.getCell(previousRow, nextColumn),
+      this.getCell(x, previousColumn),
+      this.getCell(x, nextColumn),
+      this.getCell(nextRow, previousColumn),
+      this.getCell(nextRow, y),
+      this.getCell(nextRow, nextColumn)
+    ];
   }
 
   static copy(a, b) {
